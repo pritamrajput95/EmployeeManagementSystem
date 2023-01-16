cd3ed5658b5e8657a4809c2b7c2a2f41ea237130
Pritam Rajput <pritamrajput2022@gmail.com>
2023-01-16 20:40:43 +0530

    sagar   commited

--- src/main/java/com/spring/ems/EmployeeManagementSystemApplication.java
+++ src/main/java/com/spring/ems/EmployeeManagementSystemApplication.java
@@ -9,6 +9,7 @@ public class EmployeeManagementSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
 		System.out.println("EmployeeManagementSystemApplication.main()");
+		System.out.println("Main App");
 	}
 
 }
